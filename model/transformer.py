992349f308d54a0812718affc3f66ec9a4675797
cheolhwanyoo <50947473+cheolhwanyoo@users.noreply.github.com>
2023-07-10 17:14:35 +0900

    Update transformer.py

--- model/transformer.py
+++ model/transformer.py
@@ -9,12 +9,7 @@ class vit_Siam(nn.Module):
     def __init__(self, num_classes=2, fea_dim=768):
         super(vit_Siam, self).__init__()
         self.backbone = timm.create_model(
-            'vit_base_patch32_224', ## 주의 필요
-            #'vit_base_patch16_224',
-            #'vit_base_patch32_224_in21k',
-            #'vit_small_patch16_224',
-            #'vit_tiny_patch16_224',
-            #'vit_tiny_r_s16_p8_224',
+            'vit_base_patch32_224', 
             pretrained=True,
             num_classes=2
         )
@@ -33,7 +28,6 @@ class vit_Siam(nn.Module):
         self.classifier = nn.Linear(in_features=self.in_features, out_features=2)
 
     def forward(self, x1, x2):
-    #def forward(self, x1):
         z1 = self.backbone(x1)
         z2 = self.backbone(x2)
 
@@ -41,12 +35,8 @@ class vit_Siam(nn.Module):
         p2 = self.h(z2)
 
         logits1 = self.classifier(z1)
-        #logits2 = self.classifier(z2)
 
-
-        #averaged_logits = (logits1 + logits2) / 2.0
         metric_feature = [z1, z2, p1, p2]
 
-        #return logits1
         return logits1, metric_feature
 
